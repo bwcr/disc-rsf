842a906c85cd58c176d2f6bbc3865469387ad3e0
Bayu Wicaksono <lower_lollypop@ymail.com>
2020-10-22 17:57:44 +0700

    Update mitra.php
    Using encode_quotes

--- admin/mitra.php
+++ admin/mitra.php
@@ -102,17 +102,17 @@
                     while ($rowmitra = mysqli_fetch_array($mitra)) { ?>
                     <tr>
                       <?php $id_mitra = md5($rowmitra['id_mitra']); ?>
-                      <td><?= htmlentities($i) ?></td>
-                      <td><?= htmlentities($rowmitra['mitra']) ?></td>
-                      <td><?= htmlentities($rowmitra['username']) ?></td>
-                      <td><?= htmlentities($rowmitra['email']) ?></td>
+                      <td><?= htmlentities($i, ENT_QUOTES, 'UTF-8') ?></td>
+                      <td><?= htmlentities($rowmitra['mitra'], ENT_QUOTES, 'UTF-8') ?></td>
+                      <td><?= htmlentities($rowmitra['username'], ENT_QUOTES, 'UTF-8') ?></td>
+                      <td><?= htmlentities($rowmitra['email'], ENT_QUOTES, 'UTF-8') ?></td>
                       <td><img style="max-width:200px;max-height:50px" class="shadow-sm"
                           src="../image/logo/<?= htmlentities($rowmitra["logo"]) ?>"></td>
-                      <td><a rel="modal:open" id="toggleModal<?= htmlentities($rowmitra["username"]) ?>"
+                      <td><a rel="modal:open" id="toggleModal<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"
                           href="#edit<?= htmlentities($rowmitra["username"]) ?>"><button class="mdc-button mdc-button--unelevated"
                             data-mdc-auto-init="MDCRipple"><i class="fas fa-edit"></i></button></a>
-                        <a rel=modal:open id="toggleSubModalMitra<?= htmlentities($rowmitra["username"]) ?>"
-                          href="#sub-modalDelete<?= htmlentities($rowmitra["username"]) ?>"><button id="delete'.$rowmitra["
+                        <a rel=modal:open id="toggleSubModalMitra<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"
+                          href="#sub-modalDelete<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"><button id="delete'.$rowmitra["
                             username"].'"
                             class="mdc-button mdc-button--unelevated secondary-filled-button mdc-ripple-upgraded"
                             data-mdc-auto-init="MDCRipple">
@@ -121,7 +121,7 @@
                         </a></td>
                     </tr>
                     <?php $i = $i + 1; ?>
-                    <div id="edit<?= htmlentities($rowmitra["username"]) ?>" class="modal">
+                    <div id="edit<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>" class="modal">
                       <div class="modal-header">
                         <h1 class="mdc-card__title mdc-card__title--large">Update Mitra</h1>
                       </div>
@@ -131,8 +131,8 @@
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
                               <label class="mdc-text-field w-100">
                                 <h2 class="mdc-typography--subheading1">Nama Mitra</h2>
-                                <form id="formUpdateMitra" method="POST" action="updateMitra.php?id=<?= htmlentities($id_mitra) ?>">
-                                  <input type="text" name="mitra" value="<?= htmlentities($rowmitra["mitra"]) ?>" required="required"
+                                <form id="formUpdateMitra" method="POST" action="updateMitra.php?id=<?= htmlentities($id_mitra, ENT_QUOTES, 'UTF-8') ?>">
+                                  <input type="text" name="mitra" value="<?= htmlentities($rowmitra["mitra"], ENT_QUOTES, 'UTF-8') ?>" required="required"
                                     class="mdc-text-field__input">
                                   <div class="mdc-text-field__bottom-line"></div>
                               </label>
@@ -140,7 +140,7 @@
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
                               <label class="mdc-text-field w-100">
                                 <h2 class="mdc-typography--subheading1">Username</h2>
-                                <input type="text" name="username" value="<?= htmlentities($rowmitra["username"]) ?>"
+                                <input type="text" name="username" value="<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"
                                   required="required" class="mdc-text-field__input">
                                 <div class="mdc-text-field__bottom-line"></div>
                               </label>
@@ -148,7 +148,7 @@
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
                               <label class="mdc-text-field w-100">
                                 <h2 class="mdc-typography--subheading1">Email</h2>
-                                <input type="email" name="email" value="<?= htmlentities($rowmitra["email"]) ?>" required="required"
+                                <input type="email" name="email" value="<?= htmlentities($rowmitra["email"], ENT_QUOTES, 'UTF-8') ?>" required="required"
                                   class="mdc-text-field__input">
                                 <div class="mdc-text-field__bottom-line"></div>
                               </label>
@@ -156,9 +156,9 @@
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
                               <label class="mdc-text-field w-100">
                                 <h2 class="mdc-typography--subheading1">Password</h2>
-                                <input id="password<?= htmlentities($rowmitra["username"]) ?>" type="password" name="password"
+                                <input id="password<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>" type="password" name="password"
                                   placeholder="Password..." required="required" class="mdc-text-field__input w-100"
-                                  value="<?= htmlentities($rowmitra["password"]) ?>">
+                                  value="<?= htmlentities($rowmitra["password"], ENT_QUOTES, 'UTF-8') ?>">
                                 <div class="mdc-text-field__bottom-line"></div>
                               </label>
                             </div>
@@ -169,24 +169,24 @@
                           Update
                         </button>
                         </form>
-                        <a rel=modal:open id="toggleSubModalMitra<?= htmlentities($rowmitra["username"]) ?>"
-                          href="#sub-modalDelete<?= htmlentities($rowmitra["username"]) ?>"><button id="delete<?= htmlentities($rowmitra["
-                            username"]) ?>" class="mdc-button mdc-button--stroked secondary-stroked-button"
+                        <a rel=modal:open id="toggleSubModalMitra<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"
+                          href="#sub-modalDelete<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"><button id="delete<?= htmlentities($rowmitra["
+                            username"], ENT_QUOTES, 'UTF-8') ?>" class="mdc-button mdc-button--stroked secondary-stroked-button"
                             data-mdc-auto-init="MDCRipple">
                             Hapus
                           </button>
                         </a>
-                        <div id="sub-modalDelete<?= htmlentities($rowmitra["username"]) ?>" class="modal">
+                        <div id="sub-modalDelete<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>" class="modal">
                           <section class="mdc-card__primary bg-white">
                             <div class="mdc-layout-grid">
                               <div class="mdc-layout-grid__inner">
                                 <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
                                   <p class="mdc-typography--body1">Apakah anda ingin menghapus mitra
-                                    <?= htmlentities($rowmitra["mitra"]) ?></p>
+                                    <?= htmlentities($rowmitra["mitra"], ENT_QUOTES, 'UTF-8') ?></p>
                                 </div>
                               </div>
-                              <a href="deleteMitra.php?id=<?= htmlentities($id_mitra) ?>"><button
-                                  id="delete<?= htmlentities($rowmitra["username"]) ?>"
+                              <a href="deleteMitra.php?id=<?= htmlentities($id_mitra, ENT_QUOTES, 'UTF-8') ?>"><button
+                                  id="delete<?= htmlentities($rowmitra["username"], ENT_QUOTES, 'UTF-8') ?>"
                                   class="mdc-button mdc-button--stroked secondary-stroked-button"
                                   data-mdc-auto-init="MDCRipple">
                                   Hapus
@@ -221,12 +221,12 @@
     <div id="alertModal" class="modal">
       <?php 
           if (isset($_SESSION['alert-success'])) { ?>
-            <p><?= htmlentities($_SESSION['alert-success']) ?></p>
-            <?php unset($_SESSION['alert-success']);
+            <p><?= htmlentities($_SESSION['alert-success'], ENT_QUOTES, 'UTF-8') ?></p>
+            <?php unset($_SESSION['alert-success'], ENT_QUOTES, 'UTF-8');
           }
-          elseif (isset($_SESSION['alert-failure'])) { ?>
-            <p><?= htmlentities($_SESSION['alert-failure']) ?></p>
-            <?php unset($_SESSION['alert-failure']);
+          elseif (isset($_SESSION['alert-failure'], ENT_QUOTES, 'UTF-8')) { ?>
+            <p><?= htmlentities($_SESSION['alert-failure'], ENT_QUOTES, 'UTF-8') ?></p>
+            <?php unset($_SESSION['alert-failure'], ENT_QUOTES, 'UTF-8');
           }
           ?>
       <a href="#" rel="modal:close">Dismiss</a>
